e944fc2dc0e9eb6b9d201dd69daa01e2d49def99
mehtabhazra <mehtabhazra@gmail.com>
2019-11-24 14:59:52 +0530

    Knn correction

--- Layout_Features/knn_cluster.py
+++ Layout_Features/knn_cluster.py
@@ -7,16 +7,16 @@ def cluster_no(img_test,test_folder,k):
     folder='b/'
     #lss=[]
     dist=[]
-    features1=[]
+    features1=[] # features of query image to be done
     for foldername in  os.listdir(folder):
         for filename in os.listdir(str(folder+'/'+foldername+'/')):
             img=Image.open(os.path.join(folder,foldername,filename))
-            #labels.append(img.label())
+            labels.append(img.label())      #extract labels from function for current image
             dist1=1-text_similarity(img,img_test)
-            dist2=np.sum((features1-features(img))**2)
+            dist2=np.sum((features1-features(img))**2) #spacing function to be used here 
             dist.append(dist1**2+dist2)
 
-    index=ind[np.argsort(a[ind])]
+    index=np.argsort(dist)
     maxx=np.zeros((17,1))
     for i in range(0,k):
         maxx[index[i]]+=1
