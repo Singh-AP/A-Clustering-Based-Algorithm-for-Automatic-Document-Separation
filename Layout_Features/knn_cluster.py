7b0cb9d22d5998bce15c639f37c54c8b66808715
mehtabhazra <mehtabhazra@gmail.com>
2019-11-24 14:48:43 +0530

    KNN modified

---
+++ Layout_Features/knn_cluster.py
@@ -0,0 +1,31 @@
+from text_similarity_function import text_similarity
+
+
+
+def cluster_no(img_test,test_folder,k):
+    i=0
+    folder='b/'
+    #lss=[]
+    dist=[]
+    features1=[]
+    for foldername in  os.listdir(folder):
+        for filename in os.listdir(str(folder+'/'+foldername+'/')):
+            img=Image.open(os.path.join(folder,foldername,filename))
+            #labels.append(img.label())
+            dist1=1-text_similarity(img,img_test)
+            dist2=np.sum((features1-features(img))**2)
+            dist.append(dist1**2+dist2)
+
+    index=ind[np.argsort(a[ind])]
+    maxx=np.zeros((17,1))
+    for i in range(0,k):
+        maxx[index[i]]+=1
+    maxxx=-1
+    for i in range(17):
+        if(maxxx<maxx[i]):
+            maxxx=index[i]
+
+    return maxxx
+    
+
+             
--- Layout_Features/text_similarity_matrix.py
+++ Layout_Features/text_similarity_matrix.py
@@ -6,28 +6,29 @@ from PIL import Image
 location=''
 
 i=0
-folder='a/'
-
+folder='b/'
+lss=[]
 for foldername in  os.listdir(folder):
     for filename in os.listdir(str(folder+'/'+foldername+'/')):
-        img=cv2.imread(os.path.join(folder,foldername,filename))
-        if img is not None:
-            i+=1
+        lss.append(os.path.join(folder,foldername,filename))
+        #img=Image.open(os.path.join(folder,foldername,filename))
+        #if img is not None:
+        i+=1
 
 similarity_matrix=np.zeros((i,i))
 i=0
 j=0
-for foldername in  os.listdir(folder):
-    for filename in os.listdir(str(folder+'/'+foldername+'/')):    
-        img=Image.open(os.path.join(folder,foldername,filename))
-        if img is not None:
-            for filename2 in  os.listdir(folder):
-                img2=Image.open(os.path.join(folder,filename2))
-                if img2 is not None:
-                    similarity_matrix[i][j]=text_similarity(img,img2)
-                    j+=1
-                    
-        i+=1
-    
+
+
+for x in lss:
+    img = Image.open(x)
+    j=0
+
+    for y in lss :
+        img2=Image.open(y)
+        if(i>=j):
+            similarity_matrix[i][j]=text_similarity(img,img2)
+            j+=1
+    i+=1
 print(similarity_matrix)
 
